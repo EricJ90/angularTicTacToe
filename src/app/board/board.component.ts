fa89b9138ed823117794648a0473716820d1c129
Eric <eaj1990@outlook.com>
2022-03-15 21:30:53 -0400

    finish game logic, ugly display

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -29,8 +29,10 @@ export class BoardComponent implements OnInit {
   makeMove(idx: number) {
     if (!this.squares[idx]) {
       this.squares?.splice(idx, 1, this.player);
-      this.xIsNext = ! this.xIsNext
+      this.xIsNext = !this.xIsNext
     }
+
+    this.winner = this.calculateWinner();
   }
 
   calculateWinner() {
